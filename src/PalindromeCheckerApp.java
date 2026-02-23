1fb630009e18f42e78e65083d8553ffab4630ada
sai-sutharsanan05 <saisutharsanan@gmail.com>
2026-02-23 10:12:56 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Hello Palindrome");
+        System.out.println("welcome to palindrome checker management system");
+        System.out.println("Versiom: 1.0");
+        System.out.println("system initialized successfully");
     }
 }
